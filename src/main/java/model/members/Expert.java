862e90e101ec79817929aade4fb615dcc2915656
negin <negin.s.mousavi@gmail.com>
2022-01-04 13:06:38 +0330

    add list of service for expert

--- src/main/java/model/members/Expert.java
+++ src/main/java/model/members/Expert.java
@@ -4,10 +4,14 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import model.services.Service;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Negin Mousavi
@@ -24,4 +28,6 @@ public class Expert extends User {
     @Column(name = "picture", columnDefinition = "LONGBLOB")
     private byte[] picture;
     private double credit;
+    @OneToMany
+    private List<Service> services = new ArrayList<>();
 }
