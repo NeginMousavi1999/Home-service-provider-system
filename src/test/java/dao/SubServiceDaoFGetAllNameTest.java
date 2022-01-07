236672f09645f7c0ae7c232e14239dc8d1bbdc03
negin <negin.s.mousavi@gmail.com>
2022-01-07 19:27:59 +0330

    use findall instead of getallnames

--- src/test/java/dao/SubServiceDaoFGetAllNameTest.java
+++ src/test/java/dao/SubServiceDaoFGetAllNameTest.java
@@ -1,11 +1,13 @@
 package dao;
 
 import config.DaoSpringConfig;
+import model.services.SubService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -23,7 +25,7 @@ public class SubServiceDaoFGetAllNameTest {
 
     @Test
     void callGetAllName_WhenTableIsNotEmpty_ThenReturnTrueResponse() {
-        List<String> allName = subServiceDao.getAllName();
+        List<String> allName = subServiceDao.returnAll().stream().map(SubService::getName).collect(Collectors.toList());
         assertTrue(allName.size() > 0);
     }
 }
